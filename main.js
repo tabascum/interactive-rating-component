3c30f0d2ae0925de98082439c6a0c92ad19781e7
tabascum <marques_vasco@yahoo.es>
2023-07-04 16:55:52 +0200

    Upload finished project

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
 const button = document.querySelector("#button");
 const submitScreen = document.querySelector("#submit-card");
 const thankCard = document.querySelector("#thank-card");
+
+const rate = document.querySelectorAll(".rounded");
+const score = document.querySelector("#selected-score");
+
+rate.forEach((item) => {
+  item.addEventListener("click", () => {
+    item.setAttribute("class", "active");
+    score.innerText = item.innerText;
+    console.log(item.innerText);
+  });
+});
+
+button.addEventListener("click", () => {
+  console.log("Clicked");
+  submitScreen.style.display = "none";
+  thankCard.style.display = "flex";
+});
